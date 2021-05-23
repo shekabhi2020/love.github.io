b5fd0557c05fc84ace409a3d51e30302d84593bd
Aditi <aditipayalagarwal@gmail.com>
2021-05-23 16:26:47 +0530

    initial commit

---
+++ custom.js
@@ -0,0 +1,13 @@
+
+function ontour(){
+    window.location.href="gallery.html";
+}
+
+function btn2click(){
+    window.location.href="lastpage.html";
+}
+
+function onhome(){
+    window.location.href="index.html";
+}
+
---
+++ js/custom.js
@@ -0,0 +1,38 @@
+
+function ontour(){
+    window.location.href="gallery.html";
+}
+
+function btn2click(){
+    window.location.href="lastpage.html";
+}
+
+function onhome(){
+    window.location.href="index.html";
+}
+  var card = $('card'),
+        openB = $('open'),
+        closeB = $('close'),
+        timer = null;
+    console.log('wat', card);
+    openB.addEventListener('click', function () {
+      card.setAttribute('class', 'open-half');
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(function () {
+        card.setAttribute('class', 'open-fully');
+        timer = null;
+      }, 1000);
+    });
+  
+    closeB.addEventListener('click', function () {
+      card.setAttribute('class', 'close-half');
+      if (timer) clearTimerout(timer);
+      timer = setTimeout(function () {
+        card.setAttribute('class', '');
+        timer = null;
+      }, 1000);
+    });
+  
+
+  
+
